810e03f96ff32f9929a34f8b653923dafa4c626a
Nicholas Ferrara <nick.d.ferrara@gmail.com>
2024-06-09 17:58:44 -0400

    Make inital commit

---
+++ src/main/kotlin/com/nickdferrara/retailstoreapp/Application.kt
@@ -0,0 +1,11 @@
+package com.nickdferrara.retailstoreapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerSpringbootRetailstoreappApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerSpringbootRetailstoreappApplication>(*args)
+}
---
+++ src/test/kotlin/com/nickdferrara/retailstoreapp/ServerSpringbootRetailstoreappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nickdferrara.retailstoreapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerSpringbootRetailstoreappApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
